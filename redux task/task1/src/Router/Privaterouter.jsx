403cfbc898d5793b6355da25cbc842a9c2fe9847
raghav123patel <gurjjarraghav123@gmail.com>
2025-06-17 22:32:40 -0700

    Update Privaterouter.jsx

--- redux task/task1/src/Router/Privaterouter.jsx
+++ redux task/task1/src/Router/Privaterouter.jsx
@@ -1,10 +1,26 @@
 
-import { Navigate, Outlet } from 'react-router-dom';
+import UserList from "../Pages/users/UserList";
+import UserDetail from "../Pages/users/UserDetail";
+import UserDelete from "../Pages/users/UserDelete";
+import UserUpdate from "../Pages/users/UserUpdate";
+import { Navigate } from "react-router-dom";
 
-const PrivateRouter = () => {
-  const token = localStorage.getItem("token");
-
-  return token ? <Outlet /> : <Navigate to="/" replace />;
-};
-
-export default PrivateRouter;
+const privateRoutes = (token) => [
+  {
+    path: "/list",
+    element: token ? <UserList /> : <Navigate to="/login" />,
+  },
+  {
+    path: "user/:id",
+    element: token ? <UserDetail /> : <Navigate to="/login" />,
+  },
+  {
+    path: "/delete/:id",
+    element: token ? <UserDelete /> : <Navigate to="/login" />,
+  },
+  {
+    path: "/user/update/:id",
+    element: token ? <UserUpdate /> : <Navigate to="/login" />,
+  },
+];
+export default privateRoutes;
