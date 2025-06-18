f496b112095a79980c09cc5ee3dadf2cabc4a674
raghav123patel <gurjjarraghav123@gmail.com>
2025-06-17 22:36:27 -0700

    Update App.jsx

--- redux task/task1/src/App.jsx
+++ redux task/task1/src/App.jsx
@@ -2,16 +2,33 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { RouterProvider } from "react-router-dom";
-
-import appRoutes from "./Router/Routes";
+import { useState, useEffect } from "react";
+import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
+import  publicRoutes  from "./Router/publicRoutes";
+import  privateRoutes  from "./Router/privateRoutes";
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={appRoutes} />
-    </>
-  );
+  const getToken = localStorage.getItem("token");
+  const [token, setToken] = useState(getToken);
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: token ? <Navigate to="/list" /> : <Navigate to="/login" />,
+    },
+    ...publicRoutes({ setToken }), 
+    ...privateRoutes({ token }),  
+  ]);
+
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
