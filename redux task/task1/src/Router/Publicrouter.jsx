cd8a3e0141868bc0251f8ccf24e148fdac43f493
raghav123patel <gurjjarraghav123@gmail.com>
2025-06-17 22:34:10 -0700

    Update Publicrouter.jsx

--- redux task/task1/src/Router/Publicrouter.jsx
+++ redux task/task1/src/Router/Publicrouter.jsx
@@ -1,10 +1,35 @@
 
-import { Navigate, Outlet } from 'react-router-dom';
+import LoginForm from "../Pages/auth/LoginForm";
+import  RegisterForm  from "../Pages/auth/RegisterForm";
+import ForgotPassword  from "../Pages/auth/ForgotPassword";
+import  ResetPassword from "../Pages/auth/ResetPassword";
+import  VerifyEmail  from "../Pages/auth/VerifyEmail";
+import { Navigate}  from "react-router-dom";
 
-const PublicRouter = () => {
-  const token = localStorage.getItem("token");
-
-  return !token ? <Outlet /> : <Navigate to="/userlist" replace />;
-};
-
-export default PublicRouter;
+ const publicRoutes = () => [
+  {
+    path: "/",
+    element: <Navigate to="/login" />,
+  },
+  {
+    path: "/login",
+    element: <LoginForm />,
+  },
+  {
+    path: "/register",
+    element: <RegisterForm />,
+  },
+  {
+    path: "/verification/:token/:id",
+    element: <VerifyEmail />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/auth/reset-password/:userId/:authToken",
+    element: <ResetPassword />,
+  },
+];
+export default publicRoutes;
